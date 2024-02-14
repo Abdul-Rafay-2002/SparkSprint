646b58cbcbcb66623f85bf15a0bdc7f43b7040f3
Abdul Rafay <99833769+Abdul-Rafay-2002@users.noreply.github.com>
2024-02-15 00:00:59 +0500

    Update Categories.tsx

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -4,9 +4,7 @@ import Container from './Container';
 import Category from './Category';
 import { usePathname, useSearchParams } from 'next/navigation';
 
-interface CategoriesProps {}
-
-const Categories: React.FC<CategoriesProps> = () => {
+const Categories = () => {
 const params = useSearchParams();
 const category = params?.get('category');
 const pathName = usePathname();
