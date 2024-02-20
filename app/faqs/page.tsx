bcd594f879b658edabd8a32c9298f57d6850debf
Rafay Dev <rafaymalick68@gmail.com>
2024-02-21 00:18:24 +0500

    worked on Faqs page

--- app/faqs/page.tsx
+++ app/faqs/page.tsx
@@ -1,26 +1,44 @@
-'use client'
+'use client';
 
 import { faqs } from '@/utils/Faqs';
 import SubpageTemplate from '../components/SubpageTemplate';
 import { Accordion } from 'react-accordion-ts';
+import 'react-accordion-ts/src/panel.css';
 
 interface AccordianProps {
 	duration: number;
-    open?: boolean;
+	open?: number;
 	multiple: boolean;
 }
 
 const FAQS: React.FC<AccordianProps> = ({ duration, multiple, open }) => {
+	function renderContent(content: any) {
+		// Check if the content has HTML tags
+		if (/<[^>]*>/i.test(content)) {
+			return <div dangerouslySetInnerHTML={{ __html: content }} />;
+		} else {
+			return <span>{content}</span>;
+		}
+	}
+
+	// Render FAQs with HTML content conditionally
+	const faqsWithRenderedContent = faqs.map((faq) => ({
+		...faq,
+		content: renderContent(faq.content),
+	}));
+
+
 	return (
 		<SubpageTemplate
 			pageTitle='Frequently Asked Questions'
-			pageDescription='Find answers to common questions about the Coding Boot Camp program and our team.'>
-			<h1 className='text-4xl font-bold'>Frequently Asked Questions</h1>
-
-			<div>
-				
-					<Accordion duration={400} multiple={false} items={faqs} />
-				
+			pageDescription='Find answers to commonly asked questions about Sparksprint'>
+			<div className='max-w-[1020px] mx-auto py-20 w-full '>
+				<Accordion
+					duration={400}
+					multiple={false}
+					items={faqsWithRenderedContent}
+					open={open}
+				/>
 			</div>
 		</SubpageTemplate>
 	);
--- utils/Faqs.ts
+++ utils/Faqs.ts
@@ -1,16 +1,21 @@
 export const faqs = [
     {
-        title: 'Awesome title',
-        content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        title: "How do I create an account on Sparksprint?",
+        content: "To create an account on Sparksprint, simply visit our website and click on the 'User' button. Follow the prompts to enter your email address, create a password, and complete the registration process. Once done, you'll have full access to Sparksprint's features and functionalities.",
     },
     {
-        title: 'Awesome title',
-        content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        title: 'What are the main features of Sparksprint?',
+        content: 'Sparksprint offers a wide range of features to enhance your digital shopping, including sucurely create an account, purshaed, view orders tools. Explore our platform to discover how Sparksprint can streamline your purchasing processes.',
         open: true
     },
     {
-        title: 'Awesome title',
-        content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        title: 'How does billing work on Sparksprint?',
+        content: "<p>Please ensure that the billing address provided matches the address associated with your credit/debit card or chosen payment method. This helps us maintain security and accuracy in processing your payments. Kindly provide the following details for accurate billing:</p><ol><li>Full Name: Please enter the full name as it appears on your credit/debit card or billing statement.</li><li>Billing Address Line 1: Include the street address, apartment/unit number, or any necessary details for accurate delivery.</li><li>Billing Address Line 2: If applicable, include additional address details such as building name, suite number, or floor.</li><li>City: Enter the city associated with your billing address.</li><li>State / Province / Region: Provide the state, province, or region corresponding to your billing address.</li><li>ZIP / Postal Code: Input the ZIP or postal code for precise location identification.</li><li>Country: Select the country where your billing address is located.</li><li>Phone Number: A contact number ensures we can reach you quickly if any issues arise with your order.</li><li>Email Address: Your email is essential for order confirmation and communication regarding billing or account updates.</li></ol>",
+        open: true
+    },
+    {
+        title: 'Does Sparksprint integrate with other tools and platforms?',
+        content: "Yes, Sparksprint offers seamless integration with a variety of popular tools and platforms, including Google Authentication, Stripe Secure Payment integration, MongoDB Atlas Secure Database Cluster, Tailwind CSS, and more. This integration allows you to sync your Sparksprint account with your existing workflows and maximize efficiency across your digital shopping experience. ",
         open: true
     }
 ];
